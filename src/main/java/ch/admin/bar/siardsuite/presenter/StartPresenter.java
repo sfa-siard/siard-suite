5099851af03c5cdb769a27595739748e4453927d
Max Burri <burri@puzzle.ch>
2022-09-16 14:34:19 +0200

    feat: i18n for start screen buttons

--- src/main/java/ch/admin/bar/siardsuite/presenter/StartPresenter.java
+++ src/main/java/ch/admin/bar/siardsuite/presenter/StartPresenter.java
@@ -2,6 +2,7 @@ package ch.admin.bar.siardsuite.presenter;
 
 import ch.admin.bar.siardsuite.Controller;
 import ch.admin.bar.siardsuite.model.Model;
+import ch.admin.bar.siardsuite.util.I18n;
 import ch.admin.bar.siardsuite.util.View;
 import ch.admin.bar.siardsuite.view.RootStage;
 import javafx.application.Platform;
@@ -9,6 +10,8 @@ import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+
+import static ch.admin.bar.siardsuite.util.I18n.createStringBinding;
 
 public class StartPresenter extends Presenter {
 
@@ -30,5 +33,10 @@ public class StartPresenter extends Presenter {
     this.upload.setOnAction(event -> System.out.println("uplaod button pressed"));
     this.export.setOnAction(event -> System.out.println("export button pressed"));
     this.open.setOnAction(event -> System.out.println("open button pressed"));
+
+    this.archive.textProperty().bind(createStringBinding("button.archive"));
+    this.upload.textProperty().bind(createStringBinding("button.upload"));
+    this.export.textProperty().bind(createStringBinding("button.export"));
+    this.open.textProperty().bind(createStringBinding("button.open"));
   }
 }
