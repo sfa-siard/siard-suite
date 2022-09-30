b7b46301fefe2bae2ef454d1db8508b40e44c844
Stephan Girod <girod@puzzle.ch>
2022-09-30 18:09:14 +0200

    feat: init view

--- src/main/java/ch/admin/bar/siardsuite/model/View.java
+++ src/main/java/ch/admin/bar/siardsuite/model/View.java
@@ -9,7 +9,8 @@ public enum View {
   ARCHIVE_STEPPER("fxml/archive/archive-stepper-view.fxml"),
   DIALOG("fxml/dialog-view.fxml"),
   ARCHIVE_DB_DIALOG("fxml/archive/archive-db-dialog-view.fxml"),
-  ARCHIVE_ABORT_DIALOG("fxml/archive/archive-abort-dialog-view.fxml");
+  ARCHIVE_ABORT_DIALOG("fxml/archive/archive-abort-dialog-view.fxml"),
+  ARCHIVE_PREVIEW("fxml/archive/archive-preview-view.fxml");
 
   private final String viewName;
 
--- src/main/java/ch/admin/bar/siardsuite/presenter/archive/ArchivePreviewPresenter.java
+++ src/main/java/ch/admin/bar/siardsuite/presenter/archive/ArchivePreviewPresenter.java
@@ -1,2 +1,61 @@
-package ch.admin.bar.siardsuite.presenter.archive;public class ArchivePreviewPresenter {
+package ch.admin.bar.siardsuite.presenter.archive;
+
+import ch.admin.bar.siardsuite.Controller;
+import ch.admin.bar.siardsuite.component.StepperButtonBox;
+import ch.admin.bar.siardsuite.model.Model;
+import ch.admin.bar.siardsuite.model.View;
+import ch.admin.bar.siardsuite.presenter.StepperPresenter;
+import ch.admin.bar.siardsuite.util.I18n;
+import ch.admin.bar.siardsuite.view.RootStage;
+import io.github.palexdev.materialfx.controls.MFXStepper;
+import javafx.fxml.FXML;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.text.Text;
+
+public class ArchivePreviewPresenter extends StepperPresenter {
+  @FXML
+  public Text title;
+  @FXML
+  public Text text;
+  @FXML
+  public VBox leftVBox;
+  @FXML
+  public BorderPane borderPane;
+  @FXML
+  private StepperButtonBox buttonsBox;
+
+
+  @Override
+  public void init(Controller controller, Model model, RootStage stage) {
+    this.model = model;
+    this.controller = controller;
+    this.stage = stage;
+  }
+
+  @Override
+  public void init(Controller controller, Model model, RootStage stage, MFXStepper stepper) {
+    this.model = model;
+    this.controller = controller;
+    this.stage = stage;
+
+    this.title.textProperty().bind(I18n.createStringBinding("archivePreview.view.title"));
+    this.text.textProperty().bind(I18n.createStringBinding("archivePreview.view.text"));
+
+    this.buttonsBox = new StepperButtonBox();
+    this.borderPane.setBottom(buttonsBox);
+    this.setListeners(stepper);
+  }
+
+  private void setListeners(MFXStepper stepper) {
+
+    this.buttonsBox.next().setOnAction((event) -> {
+        stepper.next();
+    });
+    this.buttonsBox.previous().setOnAction((event) -> {
+      stepper.previous();
+      this.stage.setHeight(950);
+    });
+    this.buttonsBox.cancel().setOnAction((event) -> stage.openDialog(View.ARCHIVE_ABORT_DIALOG.getName()));
+  }
 }
