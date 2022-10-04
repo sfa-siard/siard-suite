4578ba4c5a3591e9a08a96324da31224f7cfef27
Jan Walker <walker@puzzle.ch>
2022-10-04 14:35:30 +0200

    recent files list

--- src/main/java/ch/admin/bar/siardsuite/presenter/OpenSiardFileDialogPresenter.java
+++ src/main/java/ch/admin/bar/siardsuite/presenter/OpenSiardFileDialogPresenter.java
@@ -2,7 +2,6 @@ package ch.admin.bar.siardsuite.presenter;
 
 import ch.admin.bar.siardsuite.Controller;
 import ch.admin.bar.siardsuite.model.Model;
-import ch.admin.bar.siardsuite.model.View;
 import ch.admin.bar.siardsuite.util.I18n;
 import ch.admin.bar.siardsuite.view.RootStage;
 import io.github.palexdev.materialfx.controls.MFXButton;
@@ -11,18 +10,22 @@ import javafx.scene.layout.HBox;
 import javafx.scene.text.Text;
 import javafx.stage.FileChooser;
 
-import javax.swing.*;
+import java.io.File;
+import java.util.List;
+import java.util.prefs.Preferences;
 
 public class OpenSiardFileDialogPresenter extends DialogPresenter {
 
     @FXML
-    public Text text;
+    protected Text text;
     @FXML
     protected MFXButton closeButton; // seems redundant
     @FXML
-    public MFXButton newConnectionButton;
+    protected MFXButton chooseFileButton;
     @FXML
     protected HBox buttonBox;
+
+    private final Preferences preferences = Preferences.userRoot().node(this.getClass().getName());
 
     @Override
     public void init(Controller controller, Model model, RootStage stage) {
@@ -33,18 +36,53 @@ public class OpenSiardFileDialogPresenter extends DialogPresenter {
         setTitle("open.siard.file.dialog.title");
         text.textProperty().bind(I18n.createStringBinding("open.siard.file.dialog.text"));
 
-        newConnectionButton.textProperty().bind(I18n.createStringBinding("archiveDbDialog.btnNewConnection"));
-        newConnectionButton.getStyleClass().setAll("button", "primary");
+        chooseFileButton.textProperty().bind(I18n.createStringBinding("open.siard.file.choose.file.button"));
+        chooseFileButton.getStyleClass().setAll("button", "secondary");
 
-        newConnectionButton.setOnAction(event -> {
+        chooseFileButton.setOnAction(event -> {
             FileChooser fileChooser = new FileChooser();
-            fileChooser.setTitle("Test");
-            fileChooser.showOpenDialog(stage);
+            fileChooser.setTitle(I18n.get("open.siard.file.choose.file.title"));
+            FileChooser.ExtensionFilter extensionFilter = new FileChooser.ExtensionFilter("SIARD files", "*.siard");
+            fileChooser.getExtensionFilters().add(extensionFilter);
+            readArchive(fileChooser.showOpenDialog(stage));
         });
 
         closeButton.setOnAction(event -> stage.closeDialog());
 
         buttonBox.getChildren().add(getCancelButton());
+    }
+
+    private void readArchive(File file) {
+        if (file != null) {
+            addRecentFile(file);
+        }
+    }
+
+    private void addRecentFile(File file) {
+        final int max_recent_files = 30;
+        final String[] recentFiles = new String[max_recent_files];
+        for (int i = 0; i < max_recent_files; i++) {
+            recentFiles[i] = preferences.get(String.valueOf(i), "");
+        }
+
+        final int k = List.of(recentFiles).indexOf(file.getAbsolutePath());
+        final int l = (k < 0) ? max_recent_files - 1 : k;
+
+        if (l > 0) {
+            for (int j = 0; j < l; j++) {
+                preferences.put(String.valueOf(j+1), recentFiles[j]);
+            }
+        }
+        preferences.put("0", file.getAbsolutePath());
+    }
+
+    private String[] getRecentFiles() {
+        final int num_recent_files = 3;
+        final String[] recentFiles = new String[num_recent_files];
+        for (int i = 0; i < num_recent_files; i++) {
+            recentFiles[i] = preferences.get(String.valueOf(i), "");
+        }
+        return recentFiles;
     }
 
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,8 +2,9 @@ module ch.admin.bar.siardsuite {
   requires javafx.controls;
   requires javafx.fxml;
   requires MaterialFX;
+    requires java.prefs;
 
-  opens ch.admin.bar.siardsuite.presenter to javafx.fxml;
+    opens ch.admin.bar.siardsuite.presenter to javafx.fxml;
   exports ch.admin.bar.siardsuite;
   opens ch.admin.bar.siardsuite.presenter.archive to javafx.fxml;
   opens ch.admin.bar.siardsuite.presenter.tree to javafx.fxml;
