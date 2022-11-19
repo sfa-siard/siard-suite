761563dd0392128517eabd0f3968f18bfd19b96d
Stephan Girod <girod@puzzle.ch>
2022-11-19 02:07:16 +0100

    feat: style and animate start page

--- src/main/java/ch/admin/bar/siardsuite/component/Icon.java
+++ src/main/java/ch/admin/bar/siardsuite/component/Icon.java
@@ -8,10 +8,22 @@ public class Icon {
   public static Image loading;
   public static Image ok;
   public static Image error;
+  public static Image siardDb;
+  public static Image siardDbRed;
+  public static Image archive;
+  public static Image archiveRed;
+  public static Image export;
+  public static Image exportRed;
 
   static {
     loading = new Image(String.valueOf(SiardApplication.class.getResource("icons/loading.png")));
     ok = new Image(String.valueOf(SiardApplication.class.getResource("icons/ok_check.png")));
     error = new Image(String.valueOf(SiardApplication.class.getResource("icons/x-circle-red.png")));
+    siardDb = new Image(String.valueOf(SiardApplication.class.getResource("icons/siard-db.png")));
+    siardDbRed = new Image(String.valueOf(SiardApplication.class.getResource("icons/siard-db_red.png")));
+    archive = new Image(String.valueOf(SiardApplication.class.getResource("icons/archive.png")));
+    archiveRed = new Image(String.valueOf(SiardApplication.class.getResource("icons/archive_red.png")));
+    export = new Image(String.valueOf(SiardApplication.class.getResource("icons/export.png")));
+    exportRed = new Image(String.valueOf(SiardApplication.class.getResource("icons/export_red.png")));
   }
 }
