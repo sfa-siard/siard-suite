efd8c2a30db6941fed1a9474c4199f155c89b3ed
Max Burri <burri@puzzle.ch>
2022-12-22 13:59:29 +0100

    start migration to java 8

--- src/main/java/ch/admin/bar/siardsuite/database/DatabaseConnectionProperties.java
+++ src/main/java/ch/admin/bar/siardsuite/database/DatabaseConnectionProperties.java
@@ -5,6 +5,7 @@ import javafx.beans.property.StringProperty;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 import java.util.TreeMap;
 
 public class DatabaseConnectionProperties {
@@ -85,6 +86,51 @@ public class DatabaseConnectionProperties {
     return this.password;
   }
 
-  public record DatabaseProperties(String product, String port, String defaultUrl) {
-  }
+  public static final class DatabaseProperties {
+    private final String product;
+    private final String port;
+    private final String defaultUrl;
+
+    public DatabaseProperties(String product, String port, String defaultUrl) {
+      this.product = product;
+      this.port = port;
+      this.defaultUrl = defaultUrl;
+    }
+
+    public String product() {
+      return product;
+    }
+
+    public String port() {
+      return port;
+    }
+
+    public String defaultUrl() {
+      return defaultUrl;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+      if (obj == this) return true;
+      if (obj == null || obj.getClass() != this.getClass()) return false;
+      DatabaseProperties that = (DatabaseProperties) obj;
+      return Objects.equals(this.product, that.product) &&
+              Objects.equals(this.port, that.port) &&
+              Objects.equals(this.defaultUrl, that.defaultUrl);
+    }
+
+    @Override
+    public int hashCode() {
+      return Objects.hash(product, port, defaultUrl);
+    }
+
+    @Override
+    public String toString() {
+      return "DatabaseProperties[" +
+              "product=" + product + ", " +
+              "port=" + port + ", " +
+              "defaultUrl=" + defaultUrl + ']';
+    }
+
+    }
 }
--- src/main/java/ch/admin/bar/siardsuite/model/TableSearchBase.java
+++ src/main/java/ch/admin/bar/siardsuite/model/TableSearchBase.java
@@ -3,5 +3,43 @@ package ch.admin.bar.siardsuite.model;
 import javafx.scene.control.TableView;
 import java.util.LinkedHashSet;
 import java.util.Map;
+import java.util.Objects;
 
-public record TableSearchBase(TableView<Map> tableView, LinkedHashSet<Map> rows) { }
+public final class TableSearchBase {
+    private final TableView<Map> tableView;
+    private final LinkedHashSet<Map> rows;
+
+    public TableSearchBase(TableView<Map> tableView, LinkedHashSet<Map> rows) {
+        this.tableView = tableView;
+        this.rows = rows;
+    }
+
+    public TableView<Map> tableView() {
+        return tableView;
+    }
+
+    public LinkedHashSet<Map> rows() {
+        return rows;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (obj == this) return true;
+        if (obj == null || obj.getClass() != this.getClass()) return false;
+        TableSearchBase that = (TableSearchBase) obj;
+        return Objects.equals(this.tableView, that.tableView) &&
+                Objects.equals(this.rows, that.rows);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(tableView, rows);
+    }
+
+    @Override
+    public String toString() {
+        return "TableSearchBase[" +
+                "tableView=" + tableView + ", " +
+                "rows=" + rows + ']';
+    }
+}
