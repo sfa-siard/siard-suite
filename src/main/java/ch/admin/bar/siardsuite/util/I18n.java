d4d611b693ee1a08377fe809b2427fcd1bbaace2
Max Burri <burri@puzzle.ch>
2023-05-16 09:10:21 +0200

    refactor: reformat code, add deprecation/ todo

--- src/main/java/ch/admin/bar/siardsuite/util/I18n.java
+++ src/main/java/ch/admin/bar/siardsuite/util/I18n.java
@@ -12,76 +12,86 @@ import javafx.beans.property.StringProperty;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
 public class I18n {
 
-  private static final ObjectProperty<Locale> locale;
+    private static final ObjectProperty<Locale> locale;
 
-  public static Locale getLocale() {
-    return locale.get();
-  }
-
-  public static void setLocale(final Locale locale) {
-    localeProperty().set(locale);
-    Locale.setDefault(locale);
-  }
-
-  public static ObjectProperty<Locale> localeProperty() {
-    return locale;
-  }
-
-  static {
-    locale = new SimpleObjectProperty<>(getDefaultLocale());
-    locale.addListener((observable, oldValue, newValue) -> Locale.setDefault(newValue));
-  }
-
-  public static Locale getDefaultLocale() {
-    Locale defaultLocale = Locale.getDefault();
-    return getSupportedLocales().contains(defaultLocale) ? defaultLocale : Locale.ENGLISH;
-  }
-
-  public static List<Locale> getSupportedLocales() {
-    List<Locale> locales = new ArrayList<>();
-    locales.add(Locale.ENGLISH);
-    locales.add(Locale.GERMAN);
-    locales.add(Locale.FRENCH);
-    locales.add(Locale.ITALIAN);
-    return locales;
-  }
-
-  private static final String BASE_NAME = "ch/admin/bar/siardsuite/i18n/messages";
-
-  public static String get(final String key, final Object... args) {
-    ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, getLocale());
-    String translation = bundle.containsKey(key) ? bundle.getString(key) : key;
-    return String.format(translation, args);
-  }
-
-  @Deprecated
-  public static StringBinding createStringBinding(final String key, final Object... args) {
-    return Bindings.createStringBinding(() -> get(key, args), locale);
-  }
-  public static void bind(StringProperty stringProperty, final String key, final Object... args) {
-    stringProperty.bind(I18n.createStringBinding(key, args));
-  }
-
-  public static ObjectBinding<TreeAttributeWrapper> createTreeAtributeWrapperBinding(final String key, final TreeContentView type, DatabaseObject databaseObject, final Object... args) {
-    return Bindings.createObjectBinding(() -> new TreeAttributeWrapper(get(key, args), type, databaseObject), locale);
-  }
-
-  public static String getLocaleDate(final LocalDate date) {
-    final String pattern;
-    if (getLocale().equals(Locale.ENGLISH)) {
-      pattern = "dd MMM yyyy";
-    } else {
-      pattern = "dd. MMM yyyy";
+    public static Locale getLocale() {
+        return locale.get();
     }
-    return date.format(DateTimeFormatter.ofPattern(pattern, getLocale()));
-  }
 
-  public static String getLocaleDate(final String yyyy_mm_dd) {
-    final LocalDate date = LocalDate.parse(yyyy_mm_dd);
-    return getLocaleDate(date);
-  }
+    public static void setLocale(final Locale locale) {
+        localeProperty().set(locale);
+        Locale.setDefault(locale);
+    }
+
+    public static ObjectProperty<Locale> localeProperty() {
+        return locale;
+    }
+
+    static {
+        locale = new SimpleObjectProperty<>(getDefaultLocale());
+        locale.addListener((observable, oldValue, newValue) -> Locale.setDefault(newValue));
+    }
+
+    public static Locale getDefaultLocale() {
+        Locale defaultLocale = Locale.getDefault();
+        return getSupportedLocales().contains(defaultLocale) ? defaultLocale : Locale.ENGLISH;
+    }
+
+    public static List<Locale> getSupportedLocales() {
+        List<Locale> locales = new ArrayList<>();
+        locales.add(Locale.ENGLISH);
+        locales.add(Locale.GERMAN);
+        locales.add(Locale.FRENCH);
+        locales.add(Locale.ITALIAN);
+        return locales;
+    }
+
+    private static final String BASE_NAME = "ch/admin/bar/siardsuite/i18n/messages";
+
+    public static String get(final String key, final Object... args) {
+        ResourceBundle bundle = ResourceBundle.getBundle(BASE_NAME, getLocale());
+        String translation = bundle.containsKey(key) ? bundle.getString(key) : key;
+        return String.format(translation, args);
+    }
+
+    @Deprecated
+    public static StringBinding createStringBinding(final String key, final Object... args) {
+        return Bindings.createStringBinding(() -> get(key, args), locale);
+    }
+
+    public static void bind(StringProperty stringProperty, final String key, final Object... args) {
+        stringProperty.bind(I18n.createStringBinding(key, args));
+    }
+
+    // TODO: move to TreeItemFactory and make it private?
+    @Deprecated
+    public static ObjectBinding<TreeAttributeWrapper> createTreeAtributeWrapperBinding(final String key,
+                                                                                       final TreeContentView type,
+                                                                                       DatabaseObject databaseObject,
+                                                                                       final Object... args) {
+        return Bindings.createObjectBinding(() -> new TreeAttributeWrapper(get(key, args), type, databaseObject),
+                                            locale);
+    }
+
+    public static String getLocaleDate(final LocalDate date) {
+        final String pattern;
+        if (getLocale().equals(Locale.ENGLISH)) {
+            pattern = "dd MMM yyyy";
+        } else {
+            pattern = "dd. MMM yyyy";
+        }
+        return date.format(DateTimeFormatter.ofPattern(pattern, getLocale()));
+    }
+
+    public static String getLocaleDate(final String yyyy_mm_dd) {
+        final LocalDate date = LocalDate.parse(yyyy_mm_dd);
+        return getLocaleDate(date);
+    }
 }
