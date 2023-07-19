0ce66d0ec6551a89efbf4237348d8e71c4e42e68
Max Burri <burri@puzzle.ch>
2023-07-19 08:56:26 +0200

    fix: handle (var)binary from postgresql/mssql as binary data

--- src/test/java/ch/admin/bar/siardsuite/model/facades/PreTypeFacadeTest.java
+++ src/test/java/ch/admin/bar/siardsuite/model/facades/PreTypeFacadeTest.java
@@ -1,10 +1,12 @@
 package ch.admin.bar.siardsuite.model.facades;
 
 import ch.enterag.sqlparser.datatype.enums.PreType;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class PreTypeFacadeTest {
 
@@ -12,8 +14,12 @@ class PreTypeFacadeTest {
     void isBlob_false() {
         // given
         Arrays.stream(PreType.values()).forEach(preType -> {
-                                                    if (PreType.BLOB.equals(preType) || PreType.CLOB.equals(preType) || PreType.NCLOB.equals(preType)) return;
-                                                    Assertions.assertFalse(new PreTypeFacade(preType.getSqlType()).isBlob());
+                                                    if (PreType.BLOB.equals(preType) ||
+                                                            PreType.CLOB.equals(preType) ||
+                                                            PreType.NCLOB.equals(preType) ||
+                                                            PreType.VARBINARY.equals(preType) ||
+                                                            PreType.BINARY.equals(preType)) return;
+                                                    assertFalse(new PreTypeFacade(preType.getSqlType()).isBlob());
                                                 }
 
         );
@@ -21,8 +27,10 @@ class PreTypeFacadeTest {
 
     @Test
     void isBlob_true() {
-        Assertions.assertTrue(new PreTypeFacade(PreType.BLOB.getSqlType()).isBlob());
-        Assertions.assertTrue(new PreTypeFacade(PreType.CLOB.getSqlType()).isBlob());
-        Assertions.assertTrue(new PreTypeFacade(PreType.NCLOB.getSqlType()).isBlob());
+        assertTrue(new PreTypeFacade(PreType.BLOB.getSqlType()).isBlob());
+        assertTrue(new PreTypeFacade(PreType.CLOB.getSqlType()).isBlob());
+        assertTrue(new PreTypeFacade(PreType.NCLOB.getSqlType()).isBlob());
+        assertTrue(new PreTypeFacade(PreType.BINARY.getSqlType()).isBlob());
+        assertTrue(new PreTypeFacade(PreType.VARBINARY.getSqlType()).isBlob());
     }
 }
