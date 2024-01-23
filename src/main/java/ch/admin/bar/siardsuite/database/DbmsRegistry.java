9b6577a0d4eafccc5ae30a380d074558451e13f0
Simon Bühlmann <sdbuehlmann@gmail.com>
2024-01-23 12:33:42 +0100

    Tested and fixed all jdbc encoder and decoder

--- src/main/java/ch/admin/bar/siardsuite/database/DbmsRegistry.java
+++ src/main/java/ch/admin/bar/siardsuite/database/DbmsRegistry.java
@@ -37,19 +37,25 @@ public class DbmsRegistry {
                     .id(DbmsId.of("db2"))
                     .driverClassName("ch.admin.bar.siard2.jdbc.Db2Driver")
                     .jdbcConnectionStringEncoder(config -> String.format(
-                            "jdbc:db2://%s:%s/%s", // example: "jdbc:db2://myhost:5021/mydb:user=dbadm;password=dbadm;"
+                            "jdbc:db2://%s:%s/%s%s",
                             config.getHost(),
                             config.getPort(),
-                            config.getDbName()))
+                            config.getDbName(),
+                            config.getOptions()
+                                    .map(optionsString -> "?" + optionsString)
+                                    .orElse("")))
                     .jdbcConnectionStringDecoder(encoded -> {
                         val splitEncoded = encoded.split(":");
-                        val splitPortAndDbName = splitEncoded[3].split("/", 2);
-                        val port = splitPortAndDbName[0].replace("//", "");
+                        val splitPortAndDbNameWithOptions = splitEncoded[3].split("/", 2);
+                        val splitDbNameAndOptions = splitPortAndDbNameWithOptions[1].split("\\?", 2);
 
                         return ServerBasedDbmsConnectionProperties.builder()
-                                .host(splitEncoded[2])
-                                .port(port)
-                                .dbName(splitPortAndDbName[1])
+                                .host(splitEncoded[2].replace("//", ""))
+                                .port(splitPortAndDbNameWithOptions[0])
+                                .dbName(splitDbNameAndOptions[0])
+                                .options(splitDbNameAndOptions.length > 1 ? Optional.of(splitDbNameAndOptions[1]) : Optional.empty())
+                                .user("")
+                                .password("")
                                 .build();
                     })
                     .examplePort("50000")
@@ -62,19 +68,25 @@ public class DbmsRegistry {
                     .id(DbmsId.of("mysql"))
                     .driverClassName("ch.admin.bar.siard2.jdbc.MySqlDriver")
                     .jdbcConnectionStringEncoder(config -> String.format(
-                            "jdbc:mysql://%s:%s/%s", // example: "jdbc:mysql://localhost:8080/test?user=minty&password=greatsqldb"
+                            "jdbc:mysql://%s:%s/%s%s",
                             config.getHost(),
                             config.getPort(),
-                            config.getDbName()))
+                            config.getDbName(),
+                            config.getOptions()
+                                    .map(optionsString -> "?" + optionsString)
+                                    .orElse("")))
                     .jdbcConnectionStringDecoder(encoded -> {
                         val splitEncoded = encoded.split(":");
                         val splitPortAndDbNameWithOptions = splitEncoded[3].split("/", 2);
                         val splitDbNameAndOptions = splitPortAndDbNameWithOptions[1].split("\\?", 2);
 
                         return ServerBasedDbmsConnectionProperties.builder()
-                                .host(splitEncoded[2])
+                                .host(splitEncoded[2].replace("//", ""))
                                 .port(splitPortAndDbNameWithOptions[0])
                                 .dbName(splitDbNameAndOptions[0])
+                                .options(splitDbNameAndOptions.length > 1 ? Optional.of(splitDbNameAndOptions[1]) : Optional.empty())
+                                .user("")
+                                .password("")
                                 .build();
                     })
                     .examplePort("3306")
@@ -87,19 +99,25 @@ public class DbmsRegistry {
                     .id(DbmsId.of("oracle"))
                     .driverClassName("ch.admin.bar.siard2.jdbc.OracleDriver")
                     .jdbcConnectionStringEncoder(config -> String.format(
-                            "jdbc:oracle:thin:@%s:%s/%s",
+                            "jdbc:oracle:thin:@//%s:%s/%s%s",
                             config.getHost(),
                             config.getPort(),
-                            config.getDbName()))
-                    .jdbcConnectionStringDecoder(encoded -> { // TODO
+                            config.getDbName(),
+                            config.getOptions()
+                                    .map(optionsString -> "?" + optionsString)
+                                    .orElse("")))
+                    .jdbcConnectionStringDecoder(encoded -> {
                         val splitEncoded = encoded.split(":");
-                        val splitPortAndDbNameWithOptions = splitEncoded[3].split("/", 2);
+                        val splitPortAndDbNameWithOptions = splitEncoded[4].split("/", 2);
                         val splitDbNameAndOptions = splitPortAndDbNameWithOptions[1].split("\\?", 2);
 
                         return ServerBasedDbmsConnectionProperties.builder()
-                                .host(splitEncoded[2])
+                                .host(splitEncoded[3].replace("@//", ""))
                                 .port(splitPortAndDbNameWithOptions[0])
                                 .dbName(splitDbNameAndOptions[0])
+                                .options(splitDbNameAndOptions.length > 1 ? Optional.of(splitDbNameAndOptions[1]) : Optional.empty())
+                                .user("")
+                                .password("")
                                 .build();
                     })
                     .examplePort("1521")
@@ -112,10 +130,13 @@ public class DbmsRegistry {
                     .id(DbmsId.of("postgresql"))
                     .driverClassName("ch.admin.bar.siard2.jdbc.PostgresDriver")
                     .jdbcConnectionStringEncoder(config -> String.format(
-                            "jdbc:postgresql://%s:%s/%s", // example: "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true"
+                            "jdbc:postgresql://%s:%s/%s%s", // example: "jdbc:postgresql://localhost/test?user=fred&password=secret&ssl=true"
                             config.getHost(),
                             config.getPort(),
-                            config.getDbName()))
+                            config.getDbName(),
+                            config.getOptions()
+                                    .map(optionsString -> "?" + optionsString)
+                                    .orElse("")))
                     .jdbcConnectionStringDecoder(encoded -> { // TODO
                         val splitEncoded = encoded.split(":");
                         val splitPortAndDbNameWithOptions = splitEncoded[3].split("/", 2);
@@ -143,19 +164,36 @@ public class DbmsRegistry {
                     .id(DbmsId.of("mssql"))
                     .driverClassName("ch.admin.bar.siard2.jdbc.MsSqlDriver")
                     .jdbcConnectionStringEncoder(config -> String.format(
-                            "jdbc:sqlserver://%s:%s;databaseName=%s",
+                            "jdbc:sqlserver://%s:%s;databaseName=%s%s",
                             config.getHost(),
                             config.getPort(),
-                            config.getDbName()))
+                            config.getDbName(),
+                            config.getOptions()
+                                    .map(optionsString -> ";" + optionsString)
+                                    .orElse("")))
                     .jdbcConnectionStringDecoder(encoded -> { // TODO
                         val splitEncoded = encoded.split(":");
-                        val splitPortAndDbNameWithOptions = splitEncoded[3].split("/", 2);
-                        val splitDbNameAndOptions = splitPortAndDbNameWithOptions[1].split("\\?", 2);
+                        val splitPortAndDbNameWithOptions = splitEncoded[3].split(";");
+
+                        val dbName = Arrays.stream(splitPortAndDbNameWithOptions)
+                                .skip(1) // contains port
+                                .filter(s -> s.startsWith("databaseName="))
+                                .map(s -> s.replace("databaseName=", ""))
+                                .findAny()
+                                .orElseThrow(() -> new IllegalArgumentException("Missing database name"));
+
+                        val options =  Arrays.stream(splitPortAndDbNameWithOptions)
+                                .skip(1) // contains port
+                                .filter(s -> !s.startsWith("databaseName="))
+                                .collect(Collectors.joining(";"));
 
                         return ServerBasedDbmsConnectionProperties.builder()
-                                .host(splitEncoded[2])
+                                .host(splitEncoded[2].replace("//", ""))
                                 .port(splitPortAndDbNameWithOptions[0])
-                                .dbName(splitDbNameAndOptions[0])
+                                .dbName(dbName)
+                                .options(Optional.of(options))
+                                .user("")
+                                .password("")
                                 .build();
                     })
                     .examplePort("1433")
--- src/test/java/ch/admin/bar/siardsuite/database/DbmsRegistryTest.java
+++ src/test/java/ch/admin/bar/siardsuite/database/DbmsRegistryTest.java
@@ -1,33 +1,187 @@
 package ch.admin.bar.siardsuite.database;
 
 import ch.admin.bar.siardsuite.database.model.DbmsId;
+import ch.admin.bar.siardsuite.database.model.FileBasedDbms;
+import ch.admin.bar.siardsuite.database.model.FileBasedDbmsConnectionProperties;
 import ch.admin.bar.siardsuite.database.model.ServerBasedDbms;
 import ch.admin.bar.siardsuite.database.model.ServerBasedDbmsConnectionProperties;
 import lombok.val;
 import org.junit.jupiter.api.Test;
 import org.testfx.assertions.api.Assertions;
 
+import java.io.File;
 import java.util.Optional;
 
 public class DbmsRegistryTest {
 
+    private static final ServerBasedDbmsConnectionProperties EXAMPLE_PROPERTIES = ServerBasedDbmsConnectionProperties.builder()
+            .host("db.host.org")
+            .port("1234")
+            .dbName("test")
+            .options(Optional.of("option=value"))
+            .user("")
+            .password("")
+            .build();
+
+    private static final String EXPECTED_JDBC_URL_FOR_POSTGRESQL = "jdbc:postgresql://db.host.org:1234/test?option=value";
+    private static final String EXPECTED_JDBC_URL_FOR_ORACLE = "jdbc:oracle:thin:@//db.host.org:1234/test?option=value";
+    private static final String EXPECTED_JDBC_URL_FOR_MYSQL = "jdbc:mysql://db.host.org:1234/test?option=value";
+    private static final String EXPECTED_JDBC_URL_FOR_MSSQL = "jdbc:sqlserver://db.host.org:1234;databaseName=test;option=value";
+    private static final String EXPECTED_JDBC_URL_FOR_DB2 = "jdbc:db2://db.host.org:1234/test?option=value";
+
+    private static final FileBasedDbmsConnectionProperties EXAMPLE_FILE_BASEDPROPERTIES = new FileBasedDbmsConnectionProperties(
+            new File("C:\\i\\am\\a\\file.accdb")
+    );
+
+    private static final String EXPECTED_JDBC_URL_FOR_MSACCESS = "jdbc:access:C:\\i\\am\\a\\file.accdb";
+
     @Test
-    public void test() throws Exception {
+    public void decode_forPostgresql_expectDecodedProperties() throws Exception {
         // given
         final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("postgresql"));
 
         // when
-        val properties = dbms.getJdbcConnectionStringDecoder().apply("jdbc:postgresql://db.host.org:1234/test?ssl=true");
+        val properties = dbms.getJdbcConnectionStringDecoder().apply(EXPECTED_JDBC_URL_FOR_POSTGRESQL);
 
         // then
         Assertions.assertThat(properties)
-                        .isEqualTo(ServerBasedDbmsConnectionProperties.builder()
-                                .host("db.host.org")
-                                .port("1234")
-                                .dbName("test")
-                                .options(Optional.ofNullable("ssl=true"))
-                                .user("")
-                                .password("")
-                                .build());
+                .isEqualTo(EXAMPLE_PROPERTIES);
+    }
+
+    @Test
+    public void encode_forPostgresql_expectEncodedUrl() {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("postgresql"));
+
+        // when
+        val url = dbms.getJdbcConnectionStringEncoder().apply(EXAMPLE_PROPERTIES);
+
+        // then
+        Assertions.assertThat(url).isEqualTo(EXPECTED_JDBC_URL_FOR_POSTGRESQL);
+    }
+
+    @Test
+    public void decode_forOracle_expectDecodedProperties() throws Exception {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("oracle"));
+
+        // when
+        val properties = dbms.getJdbcConnectionStringDecoder().apply(EXPECTED_JDBC_URL_FOR_ORACLE);
+
+        // then
+        Assertions.assertThat(properties)
+                .isEqualTo(EXAMPLE_PROPERTIES);
+    }
+
+    @Test
+    public void encode_forOracle_expectEncodedUrl() {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("oracle"));
+
+        // when
+        val url = dbms.getJdbcConnectionStringEncoder().apply(EXAMPLE_PROPERTIES);
+
+        // then
+        Assertions.assertThat(url).isEqualTo(EXPECTED_JDBC_URL_FOR_ORACLE);
+    }
+
+    @Test
+    public void decode_forMySql_expectDecodedProperties() throws Exception {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("mysql"));
+
+        // when
+        val properties = dbms.getJdbcConnectionStringDecoder().apply(EXPECTED_JDBC_URL_FOR_MYSQL);
+
+        // then
+        Assertions.assertThat(properties)
+                .isEqualTo(EXAMPLE_PROPERTIES);
+    }
+
+    @Test
+    public void encode_forMySql_expectEncodedUrl() {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("mysql"));
+
+        // when
+        val url = dbms.getJdbcConnectionStringEncoder().apply(EXAMPLE_PROPERTIES);
+
+        // then
+        Assertions.assertThat(url).isEqualTo(EXPECTED_JDBC_URL_FOR_MYSQL);
+    }
+
+    @Test
+    public void decode_forMsSql_expectDecodedProperties() throws Exception {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("mssql"));
+
+        // when
+        val properties = dbms.getJdbcConnectionStringDecoder().apply(EXPECTED_JDBC_URL_FOR_MSSQL);
+
+        // then
+        Assertions.assertThat(properties)
+                .isEqualTo(EXAMPLE_PROPERTIES);
+    }
+
+    @Test
+    public void encode_forMsSql_expectEncodedUrl() {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("mssql"));
+
+        // when
+        val url = dbms.getJdbcConnectionStringEncoder().apply(EXAMPLE_PROPERTIES);
+
+        // then
+        Assertions.assertThat(url).isEqualTo(EXPECTED_JDBC_URL_FOR_MSSQL);
+    }
+
+    @Test
+    public void decode_forDb2_expectDecodedProperties() throws Exception {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("db2"));
+
+        // when
+        val properties = dbms.getJdbcConnectionStringDecoder().apply(EXPECTED_JDBC_URL_FOR_DB2);
+
+        // then
+        Assertions.assertThat(properties)
+                .isEqualTo(EXAMPLE_PROPERTIES);
+    }
+
+    @Test
+    public void encode_forDb2_expectEncodedUrl() {
+        // given
+        final ServerBasedDbms dbms = (ServerBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("db2"));
+
+        // when
+        val url = dbms.getJdbcConnectionStringEncoder().apply(EXAMPLE_PROPERTIES);
+
+        // then
+        Assertions.assertThat(url).isEqualTo(EXPECTED_JDBC_URL_FOR_DB2);
+    }
+
+    @Test
+    public void decode_forAccess_expectDecodedProperties() throws Exception {
+        // given
+        val dbms = (FileBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("access"));
+
+        // when
+        val properties = dbms.getJdbcConnectionStringDecoder().apply(EXPECTED_JDBC_URL_FOR_MSACCESS);
+
+        // then
+        Assertions.assertThat(properties)
+                .isEqualTo(EXAMPLE_FILE_BASEDPROPERTIES);
+    }
+
+    @Test
+    public void encode_forAccess_expectEncodedUrl() {
+        // given
+        val dbms = (FileBasedDbms) DbmsRegistry.findDbmsById(DbmsId.of("access"));
+
+        // when
+        val url = dbms.getJdbcConnectionStringEncoder().apply(EXAMPLE_FILE_BASEDPROPERTIES);
+
+        // then
+        Assertions.assertThat(url).isEqualTo(EXPECTED_JDBC_URL_FOR_MSACCESS);
     }
 }
