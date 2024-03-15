9751308d3451b8676457af0f27195e8685f3dae7
Simon Bühlmann <sdbuehlmann@gmail.com>
2024-03-15 21:58:40 +0100

    Add some javadoc

--- src/main/java/ch/admin/bar/siardsuite/framework/hooks/HooksCaller.java
+++ src/main/java/ch/admin/bar/siardsuite/framework/hooks/HooksCaller.java
@@ -8,11 +8,22 @@ import org.jetbrains.annotations.Nullable;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicReference;
 
+/**
+ * Utility class which calling hooks on previously loaded views if a new view is loaded
+ * or this class is cleared.
+ */
 @Slf4j
 public class HooksCaller {
 
     private final AtomicReference<LoadedView> previouslyLoadedView = new AtomicReference<>();
 
+    /**
+     * Currently calls the hooks on the previously loaded view, if it is present and the controller
+     * implements the {@link Destructible} interface.
+     *
+     * @param loadedView The newly loaded view.
+     * @return The newly loaded view.
+     */
     public LoadedView nextView(@Nullable final LoadedView loadedView) {
         return previouslyLoadedView.updateAndGet(previouslyLoadedView -> {
             if (previouslyLoadedView != null) {
