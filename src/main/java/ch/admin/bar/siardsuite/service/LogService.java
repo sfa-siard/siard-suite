de27dd65dc0ad3df19a2d2aecf543cd8ce15627c
Simon Bühlmann <sdbuehlmann@gmail.com>
2024-03-18 09:59:39 +0100

    Implement a quick method to locate the directory containing the log files

---
+++ src/main/java/ch/admin/bar/siardsuite/service/LogService.java
@@ -0,0 +1,44 @@
+package ch.admin.bar.siardsuite.service;
+
+import ch.admin.bar.siardsuite.util.CastHelper;
+import ch.qos.logback.classic.LoggerContext;
+import ch.qos.logback.core.FileAppender;
+import lombok.val;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.util.Iterator;
+import java.util.Spliterator;
+import java.util.Spliterators;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
+
+public class LogService {
+    public File getLogFile() {
+        return findLogFile();
+    }
+
+    private static File findLogFile() {
+        val fileAppender = findFileAppender();
+
+        val file = new File(fileAppender.getFile());
+
+        return file;
+    }
+
+    private static FileAppender findFileAppender() {
+        val context = (LoggerContext) LoggerFactory.getILoggerFactory();
+
+        return context.getLoggerList().stream()
+                .flatMap(logger -> toStream(logger.iteratorForAppenders()))
+                .flatMap(CastHelper.tryCastInStream(FileAppender.class))
+                .findAny()
+                .orElseThrow(() -> new IllegalStateException("No file-appender available"));
+    }
+
+    private static <T> Stream<T> toStream(final Iterator<T> iterator) {
+        return StreamSupport.stream(
+                Spliterators.spliteratorUnknownSize(iterator, Spliterator.ORDERED),
+                false);
+    }
+}
--- src/main/java/ch/admin/bar/siardsuite/ui/RootStage.java
+++ src/main/java/ch/admin/bar/siardsuite/ui/RootStage.java
@@ -1,11 +1,17 @@
 package ch.admin.bar.siardsuite.ui;
 
-import ch.admin.bar.siardsuite.ui.component.Icon;
 import ch.admin.bar.siardsuite.framework.DialogDisplay;
 import ch.admin.bar.siardsuite.framework.ViewDisplay;
+import ch.admin.bar.siardsuite.framework.i18n.DisplayableText;
+import ch.admin.bar.siardsuite.framework.i18n.keys.I18nKeyArg;
+import ch.admin.bar.siardsuite.service.FilesService;
+import ch.admin.bar.siardsuite.service.InstallationService;
+import ch.admin.bar.siardsuite.service.LogService;
+import ch.admin.bar.siardsuite.service.ServicesFacadeBuilder;
+import ch.admin.bar.siardsuite.ui.component.Icon;
 import ch.admin.bar.siardsuite.ui.presenter.DialogPresenter;
 import ch.admin.bar.siardsuite.ui.presenter.RootPresenter;
-import ch.admin.bar.siardsuite.service.ServicesFacadeBuilder;
+import ch.enterag.utils.ProgramInfo;
 import javafx.application.Platform;
 import javafx.scene.Node;
 import javafx.scene.Scene;
@@ -13,9 +19,12 @@ import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 import lombok.val;
 
 public class RootStage extends Stage implements ViewDisplay, DialogDisplay {
+
+    private static final I18nKeyArg<String> WINDOW_TITLE = I18nKeyArg.of("window.title");
 
     private final BorderPane rootPane;
     private final BorderPane dialogPane;
@@ -25,10 +34,18 @@ public class RootStage extends Stage implements ViewDisplay, DialogDisplay {
             Platform.exit();
             System.exit(0);
         });
+        titleProperty().bind(DisplayableText.of(WINDOW_TITLE, ProgramInfo.getProgramInfo().getVersion()).bindable());
 
         val servicesFacade = new ServicesFacadeBuilder().build(this);
 
-        rootPane = RootPresenter.load(servicesFacade, this)
+        rootPane = RootPresenter.load(
+                        servicesFacade.dialogs(),
+                        servicesFacade.getService(InstallationService.class),
+                        servicesFacade.getService(FilesService.class),
+                        servicesFacade.getService(LogService.class),
+                        servicesFacade.errorHandler(),
+                        () -> fireEvent(new WindowEvent(this, WindowEvent.WINDOW_CLOSE_REQUEST))
+                )
                 .getNode();
 
         dialogPane = DialogPresenter.load()
