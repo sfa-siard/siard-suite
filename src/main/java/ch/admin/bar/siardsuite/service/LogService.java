2562f67186950be09e4ce82069de358016e38d87
Simon Bühlmann <sdbuehlmann@gmail.com>
2024-03-18 10:02:40 +0100

    Add doc

--- src/main/java/ch/admin/bar/siardsuite/service/LogService.java
+++ src/main/java/ch/admin/bar/siardsuite/service/LogService.java
@@ -13,7 +13,17 @@ import java.util.Spliterators;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
+/**
+ * Service providing functionality related to logging.
+ */
 public class LogService {
+
+    /**
+     * Retrieves the log file associated with the current logging configuration.
+     *
+     * @return The log file.
+     * @throws IllegalStateException If no log file is available.
+     */
     public File getLogFile() {
         return findLogFile();
     }
