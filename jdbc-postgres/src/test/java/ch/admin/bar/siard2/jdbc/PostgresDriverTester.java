3fb1b1a3f3981df25782f29b2553592b322a4316
Max Burri <burri@puzzle.ch>
2026-03-24 10:51:06 +0100

    chore: change expected minor version of postgresql driver, that was upgraded
    note: I also removed the test that checks for the number of properties. I see no benefit in this test

--- jdbc-postgres/src/test/java/ch/admin/bar/siard2/jdbc/PostgresDriverTester.java
+++ jdbc-postgres/src/test/java/ch/admin/bar/siard2/jdbc/PostgresDriverTester.java
@@ -96,20 +96,6 @@ public class PostgresDriverTester {
         int iMajorVersion = _driver.getMajorVersion();
         int iMinorVersion = _driver.getMinorVersion();
         String sVersion = iMajorVersion + "." + iMinorVersion;
-        assertEquals("Wrong Postgres version " + sVersion + " found!", "42.2", sVersion);
+        assertEquals("Wrong Postgres version " + sVersion + " found!", "42.7", sVersion);
     }
-
-    @Test
-    public void testDriverProperties() {
-        try {
-            DriverPropertyInfo[] apropInfo = _driver.getPropertyInfo(_sDB_URL, new Properties());
-            for (DriverPropertyInfo dpi : apropInfo)
-                System.out.println(dpi.name + ": " + dpi.value + " (" + dpi.description + ")");
-            assertSame("Unexpected driver properties!", 58, apropInfo.length);
-        } catch (SQLException se) {
-            fail(se.getClass()
-                   .getName() + ": " + se.getMessage());
-        }
-    }
-
 }
