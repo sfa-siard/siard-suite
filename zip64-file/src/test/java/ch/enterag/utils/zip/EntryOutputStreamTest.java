76c3ecf7227e5e75f838b6550e3cbea143d71c4d
Max Burri <burri@puzzle.ch>
2026-04-17 09:56:21 +0200

    chore(zip-64): use junit temp folder for test files (#165)
    * chore: use junit @TempDir instead of local file path
    
    Some tests created lots of tiny files in the tmp directory - without removing them. This caused multiple problems:
    - each test run would take longer, because the content of the folder itself was zipped...
    - intellij tried to index the folder - making it unresponsive
    
    * ref: fix (most) intellij warnings

--- zip64-file/src/test/java/ch/enterag/utils/zip/EntryOutputStreamTest.java
+++ zip64-file/src/test/java/ch/enterag/utils/zip/EntryOutputStreamTest.java
@@ -25,11 +25,12 @@ it is possible to negotiate a different license with the copyright holder.
 ======================================================================*/
 package ch.enterag.utils.zip;
 
-import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.io.TempDir;
 
 import java.io.*;
+import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.fail;
 
@@ -37,7 +38,7 @@ import static org.junit.jupiter.api.Assertions.fail;
 /** Tests EntryOutputStream.
  @author Hartwig Thomas
  */
-public class EntryOutputStreamTester {
+public class EntryOutputStreamTest {
     /** buffer size for I/O */
     private final static int iBUFFER_SIZE = 8192;
     /** zip file */
@@ -45,28 +46,18 @@ public class EntryOutputStreamTester {
     /** test files directory */
     private final static String sTESTFILES_DIRECTORY = "src/test/resources";
     /** temp directory */
-    private final static String sTEMP_DIRECTORY = "tmp";
+    @TempDir
+    Path tempDir;
 
-  /* (non-Javadoc)
-   @see junit.framework.TestCase#setUp()
-   */
+    /* (non-Javadoc)
+     @see junit.framework.TestCase#setUp()
+     */
     @BeforeEach
-    public void setUp() throws Exception {
-        /* create a file of moderate length in Temp and zip it */
-        /* temp directory */
-        File fileTemp = new File(sTEMP_DIRECTORY);
-        File fileZip = new File(fileTemp.getAbsolutePath() + File.separator + "moderate.zip");
-        if (fileZip.exists())
-            fileZip.delete();
+    public void setUp() {
+        File fileZip = tempDir.resolve("moderate.zip").toFile();
         m_sZipFile = fileZip.getAbsolutePath();
     }
 
-  /* (non-Javadoc)
-   @see junit.framework.TestCase#tearDown()
-   */
-    @AfterEach
-    public void tearDown() throws Exception {
-    }
 
     @Test
     public void testWriteRead() {
@@ -86,16 +77,13 @@ public class EntryOutputStreamTester {
 
             /* read moderate file from ZIP */
             EntryInputStream eis = zf.openEntryInputStream("moderate.txt");
-            FileOutputStream fos = new FileOutputStream(sTEMP_DIRECTORY + File.separator + "moderate.txt");
+            FileOutputStream fos = new FileOutputStream(tempDir.resolve("moderate.txt").toFile());
             for (int iRead = eis.read(buf); iRead != -1; iRead = eis.read(buf))
                 fos.write(buf, 0, iRead);
             fos.close();
             eis.close();
 
             zf.close();
-        } catch (FileNotFoundException fnfe) {
-            fail(fnfe.getClass()
-                     .getName() + ": " + fnfe.getMessage());
         } catch (IOException ie) {
             fail(ie.getClass()
                    .getName() + ": " + ie.getMessage());
