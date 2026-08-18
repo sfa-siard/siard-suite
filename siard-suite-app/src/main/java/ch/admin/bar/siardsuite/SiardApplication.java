40042876ef451d8480502866f805282eb8de509e
Max Burri <burri@puzzle.ch>
2026-08-18 17:01:44 +0200

    fix: apply global theming

--- siard-suite-app/src/main/java/ch/admin/bar/siardsuite/SiardApplication.java
+++ siard-suite-app/src/main/java/ch/admin/bar/siardsuite/SiardApplication.java
@@ -2,6 +2,9 @@ package ch.admin.bar.siardsuite;
 
 import ch.admin.bar.siardsuite.ui.RootStage;
 import ch.enterag.utils.ProgramInfo;
+import io.github.palexdev.materialfx.theming.JavaFXThemes;
+import io.github.palexdev.materialfx.theming.MaterialFXStylesheets;
+import io.github.palexdev.materialfx.theming.UserAgentBuilder;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import lombok.extern.slf4j.Slf4j;
@@ -31,6 +34,15 @@ public class SiardApplication extends Application {
         // trial to fix the bad font rendering issue from javafx
         System.setProperty("prism.lcdtext", "false");
         System.setProperty("prism.text", "t2k");
+
+        // MaterialFX 11.17.0+ dropped per-control user-agent stylesheets in favor of this global theming API
+        UserAgentBuilder.builder()
+                        .themes(JavaFXThemes.MODENA)
+                        .themes(MaterialFXStylesheets.forAssemble(true))
+                        .setDeploy(true)
+                        .setResolveAssets(true)
+                        .build()
+                        .setGlobal();
         Properties props = new Properties();
         props.load(SiardApplication.class.getResourceAsStream("version.properties"));
         String version = (String) props.get("version");
